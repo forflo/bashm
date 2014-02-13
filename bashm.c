167de8d481ff83e8f8133a32df1229711a2c1c68
Florian Mayer <mayerflorian@me.com>
2014-02-13 17:48:42 +0100

    Fixed copyright

--- bashm.c
+++ bashm.c
@@ -1,4 +1,17 @@
-/*author: florian mayer
+/*
+Copyright (C) 2014 Florian Mayer 
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses>.
+
+Additional permission under GNU GPL version 3 section 7
+
+If you modify this Program, or any covered work, by linking or combining it with basem, containing parts covered by the terms of GNU GPL version 3, the licensors of this Program grant you additional permission to convey the resulting work. 
+----
+author: florian mayer
   date: 08.02.2014
   summary: This tool, called bashm, brings shared memory functionality to the shell. It has
   a very simple CLI described in the function usage(). SHM segments can be created, destroyed, read and
